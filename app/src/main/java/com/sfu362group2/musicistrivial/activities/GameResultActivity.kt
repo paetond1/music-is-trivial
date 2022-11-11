c098492622615404eb64025c7b9dd61c5d847ec6
Peter Fan <70975284+peterfan727@users.noreply.github.com>
2022-11-10 22:17:03 -0800

    Added basic navigation logic.

--- app/src/main/java/com/sfu362group2/musicistrivial/activities/GameResultActivity.kt
+++ app/src/main/java/com/sfu362group2/musicistrivial/activities/GameResultActivity.kt
@@ -2,11 +2,32 @@ package com.sfu362group2.musicistrivial.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.sfu362group2.musicistrivial.R
 
 class GameResultActivity : AppCompatActivity() {
+
+    private lateinit var artistName: TextView
+    private lateinit var homeButton: Button
+    private lateinit var shareToFbButton: Button
+
+    // TODO: The songs displayed will take an array of top 5 songs from API
+    //  and display using a ListView. Will need to implement UI logic to
+    //  change colours based on guesses
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_result)
+
+        artistName = findViewById(R.id.artist_name)
+
+        homeButton = findViewById(R.id.button_to_home)
+        homeButton.setOnClickListener {
+            finish()
+        }
+
+        shareToFbButton = findViewById(R.id.button_share_to_Facebook)
+
     }
 }
--- app/src/main/java/com/sfu362group2/musicistrivial/activities/StatisticsActivity.kt
+++ app/src/main/java/com/sfu362group2/musicistrivial/activities/StatisticsActivity.kt
@@ -2,11 +2,38 @@ package com.sfu362group2.musicistrivial.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.sfu362group2.musicistrivial.R
 
 class StatisticsActivity : AppCompatActivity() {
+
+    private lateinit var homeButton: Button
+
+    // update the statistics using viewModel
+    private lateinit var totalGamesStat: TextView
+    private lateinit var totalScoreStat: TextView
+    private lateinit var avgScoreStat: TextView
+    private lateinit var perfectGamesStat: TextView
+    private lateinit var longestStreakStat: TextView
+    private lateinit var zeroScoreGamesStat: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statistics)
+
+        homeButton = findViewById(R.id.button_to_home)
+        homeButton.setOnClickListener {
+            finish()
+        }
+        // TODO: Programmatically update these with Database viewModel
+        totalGamesStat = findViewById(R.id.total_games_played_stat)
+        totalScoreStat = findViewById(R.id.total_score_stat)
+        avgScoreStat = findViewById(R.id.average_score_stat)
+        perfectGamesStat = findViewById(R.id.perfect_games_stat)
+        longestStreakStat = findViewById(R.id.longest_streak_stat)
+        zeroScoreGamesStat = findViewById(R.id.zero_score_games_stat)
+
     }
 }
