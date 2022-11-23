5746a97cc2285d94eda33b50792b4e80ed754011
Peter Fan <70975284+peterfan727@users.noreply.github.com>
2022-11-23 13:23:48 -0800

    Self-shuffling ListView of songs.

--- app/src/main/java/com/sfu362group2/musicistrivial/view_models/GamePlayViewModel.kt
+++ app/src/main/java/com/sfu362group2/musicistrivial/view_models/GamePlayViewModel.kt
@@ -2,6 +2,7 @@ package com.sfu362group2.musicistrivial.view_models
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.sfu362group2.musicistrivial.activities.GamePlayActivity
 import com.sfu362group2.musicistrivial.game_logic.Game
 
 class GamePlayViewModel : ViewModel() {
@@ -9,11 +10,13 @@ class GamePlayViewModel : ViewModel() {
 //    val songOptions = MutableLiveData<List<String>>(ArrayList())
 //    val listItemsClicked = MutableLiveData<ArrayList<Int>>(ArrayList())
     val game = MutableLiveData<Game>()
+    var shuffledSongs = MutableLiveData<ArrayList<GamePlayActivity.Song>>()
 
     fun setGame(
         date: String,
         artistName: String,
         allSongsInOrder: ArrayList<String>,
+        shuffledSongs: ArrayList<GamePlayActivity.Song>
     ) {
         // ensure game only initialized once
         if (game.value == null){
@@ -21,5 +24,6 @@ class GamePlayViewModel : ViewModel() {
 //            this.artistName.value = artistName
 //            this.songOptions.value = this.game.value!!.getSongOptions()
         }
+        if (shuffledSongs == null) {this.shuffledSongs.value = shuffledSongs }
     }
 }
