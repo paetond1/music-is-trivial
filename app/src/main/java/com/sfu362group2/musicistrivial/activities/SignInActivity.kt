b2dac4f8b728cef8c09736f66e247bd99c1378f3
AlexCheung <alexcheung0306@gmail.com>
2022-11-29 20:44:40 -0800

    finished displaying Google Play Games service leaderboard and achievements

--- app/src/main/java/com/sfu362group2/musicistrivial/activities/SignInActivity.kt
+++ app/src/main/java/com/sfu362group2/musicistrivial/activities/SignInActivity.kt
@@ -1,6 +1,5 @@
 package com.sfu362group2.musicistrivial.activities
 
-import android.R.attr
 import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
@@ -8,12 +7,12 @@ import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.*
 import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
+import com.google.android.gms.games.AchievementsClient
+import com.google.android.gms.games.Games
+import com.google.android.gms.games.LeaderboardsClient
+import com.google.android.gms.games.PlayGames
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
@@ -26,9 +25,13 @@ import com.sfu362group2.musicistrivial.R
 class SignInActivity : AppCompatActivity() {
 
 
+    private var achievementClient: AchievementsClient? = null
+    private var leaderboardClient: LeaderboardsClient? = null
     private lateinit var googleSignInClient: GoogleSignInClient
     private lateinit var auth: FirebaseAuth
     private lateinit var signInButton: Button
+    private lateinit var leaderboardButton: Button
+    private lateinit var achievementButton: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,94 +39,68 @@ class SignInActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sign_in)
 
         auth = Firebase.auth
+        achievementClient = PlayGames.getAchievementsClient(this)
+        leaderboardClient = PlayGames.getLeaderboardsClient(this)
 
-        //google Init
-//        initGoogleSignIn()
 
-        //play games Init
-        initPlayGamesAndSignIn()
-
-        //set up button for play game sign in or google sign in
-        signInButton = findViewById(R.id.sign_in_button)
-        signInButton.setOnClickListener {
-//            googleSignIn()
-            playGameServiceSignIn()
+        achievementButton = findViewById(R.id.achievement_button)
+        achievementButton.setOnClickListener {
+            showAchievements()
         }
-    }
-
-    // start to sign in with google
-    private fun googleSignIn() {
-        val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
-    }
-
-    // start to sign in with play games
-    private fun playGameServiceSignIn() {
-        // do we need to change the signInIntent for play game service
-        val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, PG_SIGN_IN)
-
-    }
-
-
-    //google account sign in
-    fun initGoogleSignIn() {
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken("594916045213-5plljgf61mrq3gcotnd6ftatcpcc20do.apps.googleusercontent.com")
-            .requestEmail()
-            .build()
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
-    }
-
-
-    fun initPlayGamesAndSignIn() {
-        val gso2 = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
-            .requestServerAuthCode(getString(R.string.game_service_default_web_client_id))
-            .build()
-
-        googleSignInClient = GoogleSignIn.getClient(this, gso2)
-        googleSignInClient?.silentSignIn()?.addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                println("successful silent signin")
-//                achievementClient = Games.getAchievementsClient(this, task.result!!)
-//                leaderboardsClient = Games.getLeaderboardsClient(this, task.result!!)
-            } else {
-                Log.e("Error", "signInError", task.exception)
-            }
+        leaderboardButton = findViewById(R.id.leaderboard_button)
+        leaderboardButton.setOnClickListener {
+            showLeaderboard()
         }
+//        achievementClient?.unlock("id")
+//        leaderboardClient?.submitScore("id", 100)
+
     }
+
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        println("debug: currents user: ${currentUser?.displayName}")
+
+    }
+
     @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         //case google sign in
-        if (requestCode == RC_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                // Google Sign In was successful, authenticate with Firebase
-                val account = task.getResult(ApiException::class.java)!!
-                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
-                firebaseAuthWithGoogle(account.idToken!!)
-            } catch (e: ApiException) {
-                // Google Sign In failed, update UI appropriately
-                Log.w(TAG, "Google sign in failed", e)
-            }
-        }
+//        if (requestCode == RC_SIGN_IN) {
+//            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+//            try {
+//                // Google Sign In was successful, authenticate with Firebase
+//                val account = task.getResult(ApiException::class.java)!!
+//                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+//                firebaseAuthWithGoogle(account.idToken!!)
+//            } catch (e: ApiException) {
+//                // Google Sign In failed, update UI appropriately
+//                Log.w(TAG, "Google sign in failed", e)
+//            }
+//        }
         //case play game sign in
 //        const val PG_SIGN_IN = 1002    --- random number code
         if (requestCode == PG_SIGN_IN) {
-            println("passed PG_SIGN_IN")
+            println("passed PG_SIGN_IN onActivityResult condition check")
+
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
             try {
                 // Google Sign In was successful, authenticate with Firebase
                 val account = task.getResult(ApiException::class.java)!!
                 firebaseAuthWithPlayGames(account)
+                println("debug:finish onActivityResult try scope")
             } catch (e: ApiException) {
                 // Google Sign In failed, update UI appropriately
                 Log.w(TAG, "play games sign in failed", e)
             }
         }
     }
+
     private fun firebaseAuthWithPlayGames(acct: GoogleSignInAccount) {
         // Call this function both in the silent sign-in task's OnCompleteListener and in the
         // Activity's onActivityResult handler.
@@ -136,7 +113,7 @@ class SignInActivity : AppCompatActivity() {
                     // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "signInWithCredential:success")
                     val user = auth.currentUser
-                    updateUI(user)
+                    println("debug: play games user: ${user?.displayName}")
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w(TAG, "signInWithCredential:failure", task.exception)
@@ -144,41 +121,45 @@ class SignInActivity : AppCompatActivity() {
                         baseContext, "Authentication failed.",
                         Toast.LENGTH_SHORT
                     ).show()
-                    updateUI(null)
                 }
             }
     }
 
-
-    private fun firebaseAuthWithGoogle(idToken: String) {
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
-        auth.signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithCredential:success")
-                    val user = auth.currentUser
-                    updateUI(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithCredential:failure", task.exception)
-                    updateUI(null)
-                }
-            }
+    //leaderboard show
+    private val RC_LEADERBOARD_UI = 9004
+    private fun showLeaderboard() {
+        PlayGames.getLeaderboardsClient(this)
+            .getLeaderboardIntent(getString(R.string.leaderboard_id))
+            .addOnSuccessListener { intent -> startActivityForResult(intent, RC_LEADERBOARD_UI) }
+    }
+    //achievement show
+    private val RC_ACHIEVEMENT_UI = 9003
+    private fun showAchievements() {
+        PlayGames.getAchievementsClient(this)
+            .achievementsIntent
+            .addOnSuccessListener { intent -> startActivityForResult(intent, RC_ACHIEVEMENT_UI) }
     }
 
+    //sign out
+//    fun signOut(){
+//        Firebase.auth.signOut()
+//    }
+//
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        signOut()
+//    }
 
 
-    private fun updateUI(user: FirebaseUser?) {
-        if (user != null) {
-            val intent = Intent(applicationContext, GoogleSignInActivity::class.java)
-            intent.putExtra(EXTRA_NAME, user.displayName)
-            startActivity(intent)
-        }
-    }
+
+
+
 
     companion object {
+        //code for google sign in
         const val RC_SIGN_IN = 1001
+
+        //code for play games sign in
         const val PG_SIGN_IN = 1002
         const val EXTRA_NAME = "EXTRA NAME"
     }
